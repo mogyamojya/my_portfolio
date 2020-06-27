7875ceb651c1cbe80639379fe73b54f31041a7a2
mogyamojya <mogyamojya@gmail.com>
2020-06-27 11:39:21 +0900

    チャートjs入れてみた、説明文修正してみた

--- _dev/js/main.js
+++ _dev/js/main.js
@@ -2,6 +2,7 @@ import '@babel/polyfill';
 
 import MainPaddingTop from './modules/mainPaddingTop';
 import PageTop from './modules/pageTop';
+import Chart from 'chart.js';
 
 
 // new PageTop('#pagetop', '.pagetop');
@@ -12,7 +13,54 @@ $(window).resize(function() {
   new MainPaddingTop();
 });
 
-
+window.onload=function(){
+var ctx = document.getElementById('myChart').getContext('2d');
+	var myChart = new Chart(ctx, {
+	    type: 'horizontalBar',
+	    data: {
+	        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+	        datasets: [{
+	            // label: '',
+	            data: [80, 19, 3, 5, 2, 3],
+	            backgroundColor: [
+	                'rgba(255, 99, 132, 0.2)',
+	                'rgba(54, 162, 235, 0.2)',
+	                'rgba(255, 206, 86, 0.2)',
+	                'rgba(75, 192, 192, 0.2)',
+	                'rgba(153, 102, 255, 0.2)',
+	                'rgba(255, 159, 64, 0.2)'
+	            ],
+	            borderColor: [
+	                'rgba(255, 99, 132, 1)',
+	                'rgba(54, 162, 235, 1)',
+	                'rgba(255, 206, 86, 1)',
+	                'rgba(75, 192, 192, 1)',
+	                'rgba(153, 102, 255, 1)',
+	                'rgba(255, 159, 64, 1)'
+	            ],
+	            borderWidth: 1
+	        }]
+	    },
+	    options: {
+          title: {
+              display: true,
+              fontSize: 18,
+              text: 'タイトル'
+          },
+          legend: {
+          // true or faise で切り替え
+          display: false,
+          },
+	        scales: {
+	            yAxes: [{
+	                ticks: {
+	                    beginAtZero: true
+	                }
+	            }]
+	        }
+	    }
+	});
+}
 
 //SweetScroll用の記述
 // import SweetScroll from "sweet-scroll"
