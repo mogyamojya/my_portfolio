8156ba181e9a72cc49f69f4e8c48d4c8b5ccc981
mogyamojya <mogyamojya@gmail.com>
2020-06-28 04:06:14 +0900

    修正

--- _dev/js/main.js
+++ _dev/js/main.js
@@ -18,10 +18,10 @@ var ctx = document.getElementById('myChart').getContext('2d');
 	var myChart = new Chart(ctx, {
 	    type: 'horizontalBar',
 	    data: {
-	        labels: ['HTML', 'CSS', 'Sass', 'jQuery', 'JavaScript', 'Gulp', 'Webpack', 'EJS', 'pug', 'PhotoShop', 'Illustrator', 'XD', 'Dreamweaver','VSCode', 'Subversion', 'Git', 'Word', 'Excel', 'PowerPoint', 'Teams', 'Backlog','Redmine', 'Mac', 'Windows'],
+          labels: ['HTML', 'CSS', 'Sass', 'jQuery', 'JavaScript', 'Gulp', 'Webpack', 'EJS', 'pug', 'Photoshop', 'Illustrator', 'XD', 'Dreamweaver','VSCode', 'Subversion', 'Git', 'Word', 'Excel', 'PowerPoint', 'Teams', 'Slack', 'Backlog','Redmine', 'Mac', 'Windows'],
 	        datasets: [{
 	            // label: '',
-	            data: [80, 80, 80, 40, 20, 40, 10, 70, 40, 70, 30, 40, 40, 60, 60, 30, 30, 40, 30, 80, 70, 40, 70, 40],
+	            data: [80, 80, 80, 40, 20, 40, 10, 70, 40, 70, 30, 40, 40, 60, 60, 30, 30, 40, 30, 80, 80, 70, 30, 70, 40],
 	            backgroundColor: [
 	                'rgba(255, 20, 147, 0.2)',
 	                'rgba(255, 20, 147, 0.2)',
@@ -37,6 +37,7 @@ var ctx = document.getElementById('myChart').getContext('2d');
 	                'rgba(255, 20, 147, 0.2)',
 	                'rgba(255, 20, 147, 0.2)',
 	                'rgba(255, 20, 147, 0.2)',
+                  'rgba(255, 20, 147, 0.2)',
                   'rgba(255, 20, 147, 0.2)',
                   'rgba(255, 20, 147, 0.2)',
                   'rgba(255, 20, 147, 0.2)',
@@ -63,6 +64,7 @@ var ctx = document.getElementById('myChart').getContext('2d');
 	                'rgba(255, 99, 132, 1)',
 	                'rgba(255, 99, 132, 1)',
 	                'rgba(255, 99, 132, 1)',
+                  'rgba(255, 99, 132, 1)',
                   'rgba(255, 99, 132, 1)',
                   'rgba(255, 99, 132, 1)',
                   'rgba(255, 99, 132, 1)',
